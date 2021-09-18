96a3be89e09f98d623d23038b087f8ced6f75561
greg <greg.sypek@gmail.com>
2021-09-18 20:24:57 +0200

    send image into server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
-
+const path = require("path");
 ////Server
 
 const tempOverview = fs.readFileSync(
@@ -26,12 +26,17 @@ const server = http.createServer((req, res) => {
 	const langList = langHtml.map((el) => replaceTemplate(tempLang, el)).join("");
 	const tempLeft = tempOverview.replace("{%LANGUAGES%}", langList);
 	if (pathname === "/") {
-		console.log(langHtml);
+		// console.log(langHtml);
 		res.writeHead(200, {
 			"Content-type": "text/html",
 		});
 		let output = replaceTemplate(tempLeft, dataObj);
 		res.end(output);
+	} else if (req.url.match(".png$")) {
+		const imagePath = path.join(__dirname, "public", req.url);
+		const fileStream = fs.createReadStream(imagePath);
+		res.writeHead(200, { "Content-Type": "image/png" });
+		fileStream.pipe(res);
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
